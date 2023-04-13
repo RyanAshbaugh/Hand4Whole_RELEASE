6487d7d69c91bfa7f6cbd6ce1753786da08cf3ec
Ryan Ashbaugh <rashbaugh23@gmail.com>
2023-04-13 15:00:41 -0400

    indent

--- common/utils/preprocessing.py
+++ common/utils/preprocessing.py
@@ -15,7 +15,7 @@ def load_img(path, order='RGB'):
 
     if order=='RGB':
         img = img[:,:,::-1].copy()
-    
+
     img = img.astype(np.float32)
     return img
 
@@ -28,7 +28,7 @@ def get_bbox(joint_img, joint_valid, extend_ratio=1.2):
     x_center = (xmin+xmax)/2.; width = xmax-xmin;
     xmin = x_center - 0.5 * width * extend_ratio
     xmax = x_center + 0.5 * width * extend_ratio
-    
+
     y_center = (ymin+ymax)/2.; height = ymax-ymin;
     ymin = y_center - 0.5 * height * extend_ratio
     ymax = y_center + 0.5 * height * extend_ratio
@@ -49,7 +49,7 @@ def process_bbox(bbox, img_width, img_height, do_sanitize=True):
         else:
             return None
 
-   # aspect ratio preserving bbox
+    # aspect ratio preserving bbox
     w = bbox[2]
     h = bbox[3]
     c_x = bbox[0] + w/2.
@@ -63,7 +63,7 @@ def process_bbox(bbox, img_width, img_height, do_sanitize=True):
     bbox[3] = h*1.25
     bbox[0] = c_x - bbox[2]/2.
     bbox[1] = c_y - bbox[3]/2.
-    
+
     bbox = bbox.astype(np.float32)
     return bbox
 
@@ -71,7 +71,7 @@ def get_aug_config():
     scale_factor = 0.25
     rot_factor = 30
     color_factor = 0.2
-    
+
     scale = np.clip(np.random.randn(), -1.0, 1.0) * scale_factor + 1.0
     rot = np.clip(np.random.randn(), -2.0,
                   2.0) * rot_factor if random.random() <= 0.6 else 0
@@ -87,14 +87,14 @@ def augmentation(img, bbox, data_split, enforce_flip=None):
         scale, rot, color_scale, do_flip = get_aug_config()
     else:
         scale, rot, color_scale, do_flip = 1.0, 0.0, np.array([1,1,1]), False
-    
+
     if enforce_flip is None:
         pass
     elif enforce_flip is True:
         do_flip = True
     elif enforce_flip is False:
         do_flip = False
-    
+
     img, trans, inv_trans = generate_patch_image(img, bbox, scale, rot, do_flip, cfg.input_img_shape)
     img = np.clip(img * color_scale[None,None,:], 0, 255)
     return img, trans, inv_trans, rot, do_flip
@@ -102,7 +102,7 @@ def augmentation(img, bbox, data_split, enforce_flip=None):
 def generate_patch_image(cvimg, bbox, scale, rot, do_flip, out_shape):
     img = cvimg.copy()
     img_height, img_width, img_channels = img.shape
-   
+
     bb_c_x = float(bbox[0] + 0.5*bbox[2])
     bb_c_y = float(bbox[1] + 0.5*bbox[3])
     bb_width = float(bbox[2])
@@ -153,7 +153,7 @@ def gen_trans_from_patch_cv(c_x, c_y, src_width, src_height, dst_width, dst_heig
     dst[0, :] = dst_center
     dst[1, :] = dst_center + dst_downdir
     dst[2, :] = dst_center + dst_rightdir
-    
+
     if inv:
         trans = cv2.getAffineTransform(np.float32(dst), np.float32(src))
     else:
@@ -173,9 +173,9 @@ def process_db_coord(joint_img, joint_cam, joint_valid, do_flip, img_shape, flip
             joint_img[pair[0],:], joint_img[pair[1],:] = joint_img[pair[1],:].copy(), joint_img[pair[0],:].copy()
             joint_cam[pair[0],:], joint_cam[pair[1],:] = joint_cam[pair[1],:].copy(), joint_cam[pair[0],:].copy()
             joint_valid[pair[0],:], joint_valid[pair[1],:] = joint_valid[pair[1],:].copy(), joint_valid[pair[0],:].copy()
-    
+
     # 3D data rotation augmentation
-    rot_aug_mat = np.array([[np.cos(np.deg2rad(-rot)), -np.sin(np.deg2rad(-rot)), 0], 
+    rot_aug_mat = np.array([[np.cos(np.deg2rad(-rot)), -np.sin(np.deg2rad(-rot)), 0],
     [np.sin(np.deg2rad(-rot)), np.cos(np.deg2rad(-rot)), 0],
     [0, 0, 1]], dtype=np.float32)
     joint_cam = np.dot(rot_aug_mat, joint_cam.transpose(1,0)).transpose(1,0)
@@ -186,7 +186,7 @@ def process_db_coord(joint_img, joint_cam, joint_valid, do_flip, img_shape, flip
     joint_img[:,0] = joint_img[:,0] / cfg.input_img_shape[1] * cfg.output_hm_shape[2]
     joint_img[:,1] = joint_img[:,1] / cfg.input_img_shape[0] * cfg.output_hm_shape[1]
     joint_img[:,2] = (joint_img[:,2] / (cfg.bbox_3d_size / 2) + 1)/2. * cfg.output_hm_shape[0]
-    
+
     # check truncation
     joint_trunc = joint_valid * ((joint_img[:,0] >= 0) * (joint_img[:,0] < cfg.output_hm_shape[2]) * \
                 (joint_img[:,1] >= 0) * (joint_img[:,1] < cfg.output_hm_shape[1]) * \
@@ -210,9 +210,9 @@ def process_human_model_output(human_model_param, cam_param, do_flip, img_shape,
             gender = 'neutral'
         pose = torch.FloatTensor(pose).view(-1,3); shape = torch.FloatTensor(shape).view(1,-1); # smpl parameters (pose: 72 dimension, shape: 10 dimension)
         trans = torch.FloatTensor(trans).view(1,-1) # translation vector
-        
+
         # apply camera extrinsic (rotation)
-        # merge root pose and camera rotation 
+        # merge root pose and camera rotation
         if 'R' in cam_param:
             R = np.array(cam_param['R'], dtype=np.float32).reshape(3,3)
             root_pose = pose[smpl.orig_root_joint_idx,:].numpy()
@@ -227,7 +227,7 @@ def process_human_model_output(human_model_param, cam_param, do_flip, img_shape,
             output = smpl.layer[gender](betas=shape, body_pose=body_pose, global_orient=root_pose, transl=trans)
         mesh_coord = output.vertices[0].numpy()
         joint_coord = np.dot(smpl.joint_regressor, mesh_coord)
- 
+
         # apply camera exrinsic (translation)
         # compenstate rotation (translation from origin to root joint was not cancled)
         if 'R' in cam_param and 't' in cam_param:
@@ -245,14 +245,14 @@ def process_human_model_output(human_model_param, cam_param, do_flip, img_shape,
         trans = torch.FloatTensor(trans).view(1,-1) # translation vector
 
         # apply camera extrinsic (rotation)
-        # merge root pose and camera rotation 
+        # merge root pose and camera rotation
         if 'R' in cam_param:
             R = np.array(cam_param['R'], dtype=np.float32).reshape(3,3)
             root_pose = pose[mano.orig_root_joint_idx,:].numpy()
             root_pose, _ = cv2.Rodrigues(root_pose)
             root_pose, _ = cv2.Rodrigues(np.dot(R,root_pose))
             pose[mano.orig_root_joint_idx] = torch.from_numpy(root_pose).view(3)
-      
+
         # get root joint coordinate
         root_pose = pose[mano.orig_root_joint_idx].view(1,3)
         hand_pose = torch.cat((pose[:mano.orig_root_joint_idx,:], pose[mano.orig_root_joint_idx+1:,:])).view(1,-1)
@@ -260,7 +260,7 @@ def process_human_model_output(human_model_param, cam_param, do_flip, img_shape,
             output = mano.layer[hand_type](betas=shape, hand_pose=hand_pose, global_orient=root_pose, transl=trans)
         mesh_coord = output.vertices[0].numpy()
         joint_coord = np.dot(mano.joint_regressor, mesh_coord)
-      
+
         # apply camera exrinsic (translation)
         # compenstate rotation (translation from origin to root joint was not cancled)
         if 'R' in cam_param and 't' in cam_param:
@@ -280,22 +280,22 @@ def process_human_model_output(human_model_param, cam_param, do_flip, img_shape,
         shape = torch.FloatTensor(shape).view(1,-1); expr = torch.FloatTensor(expr).view(1,-1);
         zero_pose = torch.zeros((1,3)).float() # neck and eye poses
         trans = torch.FloatTensor(trans).view(1,-1) # translation vector
- 
+
         # apply camera extrinsic (rotation)
-        # merge root pose and camera rotation 
+        # merge root pose and camera rotation
         if 'R' in cam_param:
             R = np.array(cam_param['R'], dtype=np.float32).reshape(3,3)
             root_pose = root_pose.numpy()
             root_pose, _ = cv2.Rodrigues(root_pose)
             root_pose, _ = cv2.Rodrigues(np.dot(R,root_pose))
             root_pose = torch.from_numpy(root_pose).view(1,3)
-      
+
         # get root joint coordinate
         with torch.no_grad():
             output = flame.layer(global_orient=root_pose, jaw_pose=jaw_pose, neck_pose=zero_pose, leye_pose=zero_pose, reye_pose=zero_pose, betas=shape, expression=expr, transl=trans)
         mesh_coord = output.vertices[0].numpy()
         joint_coord = output.joints[0].numpy()
-      
+
         # apply camera exrinsic (translation)
         # compenstate rotation (translation from origin to root joint was not cancled)
         if 'R' in cam_param and 't' in cam_param:
@@ -303,7 +303,7 @@ def process_human_model_output(human_model_param, cam_param, do_flip, img_shape,
             root_coord = joint_coord[flame.root_joint_idx,None,:]
             joint_coord = joint_coord - root_coord + np.dot(R, root_coord.transpose(1,0)).transpose(1,0) + t
             mesh_coord = mesh_coord - root_coord + np.dot(R, root_coord.transpose(1,0)).transpose(1,0) + t
-    
+
     joint_cam_orig = joint_coord.copy() # back-up the original one
     mesh_cam_orig = mesh_coord.copy() # back-up the original one
 
@@ -328,14 +328,14 @@ def process_human_model_output(human_model_param, cam_param, do_flip, img_shape,
     joint_img[:,0] = joint_img[:,0] / cfg.input_img_shape[1] * cfg.output_hm_shape[2]
     joint_img[:,1] = joint_img[:,1] / cfg.input_img_shape[0] * cfg.output_hm_shape[1]
     joint_img[:,2] = (joint_img[:,2] / (cfg.bbox_3d_size / 2) + 1)/2. * cfg.output_hm_shape[0]
-    
+
     # check truncation
     joint_trunc = ((joint_img[:,0] >= 0) * (joint_img[:,0] < cfg.output_hm_shape[2]) * \
                 (joint_img[:,1] >= 0) * (joint_img[:,1] < cfg.output_hm_shape[1]) * \
                 (joint_img[:,2] >= 0) * (joint_img[:,2] < cfg.output_hm_shape[0])).reshape(-1,1).astype(np.float32)
-    
+
     # 3D data rotation augmentation
-    rot_aug_mat = np.array([[np.cos(np.deg2rad(-rot)), -np.sin(np.deg2rad(-rot)), 0], 
+    rot_aug_mat = np.array([[np.cos(np.deg2rad(-rot)), -np.sin(np.deg2rad(-rot)), 0],
     [np.sin(np.deg2rad(-rot)), np.cos(np.deg2rad(-rot)), 0],
     [0, 0, 1]], dtype=np.float32)
     # coordinate
@@ -363,7 +363,7 @@ def process_human_model_output(human_model_param, cam_param, do_flip, img_shape,
         root_pose, _ = cv2.Rodrigues(root_pose)
         root_pose, _ = cv2.Rodrigues(np.dot(rot_aug_mat,root_pose))
         pose[human_model.orig_root_joint_idx] = root_pose.reshape(3)
-    
+
     # return results
     if human_model_type == 'flame':
         jaw_pose = jaw_pose.numpy().reshape(-1)

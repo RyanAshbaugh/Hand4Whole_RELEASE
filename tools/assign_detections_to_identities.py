cf6dbaaedfb5be84791ecfb9709e170ac0589710
Ryan Ashbaugh <rashbaugh23@gmail.com>
2023-05-02 00:37:03 -0400

    cleanup and finish applying assigned labels and saving assigned parameters

--- tools/assign_detections_to_identities.py
+++ tools/assign_detections_to_identities.py
@@ -1,57 +1,36 @@
 import pdb
 import os
 import sys
-import cv2
 import json
-import copy
 import argparse
 import traceback
-import numpy as np
 from tqdm import tqdm
 from glob import glob
-from pathlib import Path
 
-import torch
-import torchvision.transforms as transforms
-
-sys.path.insert(0, 'main')
-sys.path.insert(0, 'data')
 sys.path.insert(0, 'common')
-from config import cfg
-from model import get_model
 from utils.preprocessing import assign_labels
 from utils.preparation import (
     get_common_videos, keyword_filter, remove_substrings_and_strip
 )
-from utils.human_models import smpl, smpl_x, mano, flame
-from utils.vis import render_mesh, save_obj
-from collections import OrderedDict
 
 import pandas as pd
 import re
 
-transform = transforms.ToTensor()
 
-
-def run_pose_inference():
+def run_detection_identity_assignment():
     parser = argparse.ArgumentParser()
     parser.add_argument('--video', type=str,
                         default='/research/iprobe/datastore/datasets/face/iiit-dronesurf/',
                         help='Path to video or folder of videos')
-    parser.add_argument('--detection_folder', default=None, required=True, type=str)
+    parser.add_argument('--parameters_folder', default=None, required=True, type=str)
     parser.add_argument('--output_folder', default=None, required=True, type=str)
     parser.add_argument('--filter_keyword', default='', type=str)
-    parser.add_argument('--save_video', action='store_true', default=False)
-    parser.add_argument('--gpu', type=str, default='0')
-    parser.add_argument('--batch', type=int, default=64)
     args = parser.parse_args()
 
     labeled_parameter_output_folder = os.path.join(args.output_folder,
                                                    'labeled_parameters')
     if not os.path.exists(args.output_folder):
         os.makedirs(labeled_parameter_output_folder, exist_ok=True)
-
-    device = torch.device('cuda:' + args.gpu if torch.cuda.is_available() else 'cpu')
 
     videos = []
     if args.video.endswith('.mp4'):
@@ -60,25 +39,27 @@ def run_pose_inference():
         glob_path = os.path.join(args.video, '**/*.mp4')
         videos = glob(glob_path, recursive=True)
 
-    detection_files = glob(os.path.join(args.detection_folder, '**/*.json'),
-                           recursive=True)
+    parameters_files = glob(os.path.join(args.parameters_folder, '**/*.json'),
+                            recursive=True)
 
     videos = keyword_filter(videos, args.filter_keyword)
 
-    common_videos, common_detections = get_common_videos(videos, detection_files)
+    common_videos, common_parameters = get_common_videos(
+        videos, parameters_files)
 
-    assert len(common_videos) == len(common_detections), \
+    assert len(common_videos) == len(common_parameters), \
         "Number of videos and detection files should be same"
 
     substrings_to_remove = [',', '(', ')', 'Frame', 'frame']
     pattern = '|'.join(map(re.escape, substrings_to_remove))
 
-    for ii, (video, detection) in enumerate(tqdm(zip(common_videos, common_detections),
-                                                 desc="Processing videos",
-                                                 total=len(common_videos))):
+    for ii, (video, parameter_file_path) in enumerate(
+        tqdm(zip(common_videos, common_parameters),
+             desc="Processing videos",
+             total=len(common_videos))):
 
         assert video.split('/')[-1].split('.')[0] == \
-            detection.split('/')[-1].split('.')[0], \
+            parameter_file_path.split('/')[-1].split('.')[0], \
             "Video and detection file names should be same"
 
         identities = video.split('/')[-2].split(',')
@@ -111,14 +92,17 @@ def run_pose_inference():
         try:
             if not os.path.exists(output_file_path):
                 os.system(f"touch {output_file_path}")
-                assign_labels(detection, output_file_path)
+                labeled_parameters = assign_labels(parameter_file_path, labels)
+                with open(output_file_path, 'w') as f:
+                    json.dump(labeled_parameters, f)
             else:
                 print(f"\nSkipping {output_file_path}")
 
         except Exception as e:
             print(traceback.format_exc())
-            print("Error processing video {}: {}".format(video, e))
+            print("Error processing parameters {}: {}".format(
+                parameter_file_path, e))
 
 
 if __name__ == '__main__':
-    run_pose_inference()
+    run_detection_identity_assignment()
